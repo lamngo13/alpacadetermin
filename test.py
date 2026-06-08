5b5908208d892536da4d46cea1f49805fc5e8fa2
lamngo13 <69515114+lamngo13@users.noreply.github.com>
2026-06-08 15:00:13 -0400

    poc alpaca

---
+++ test.py
@@ -0,0 +1,20 @@
+import datetime
+from alpaca.data.historical import CryptoHistoricalDataClient
+from alpaca.data.requests import CryptoBarsRequest
+from alpaca.data.timeframe import TimeFrame
+
+# No keys required for crypto data
+client = CryptoHistoricalDataClient()
+# Creating request object
+request_params = CryptoBarsRequest(
+  symbol_or_symbols=["BTC/USD"],
+  timeframe=TimeFrame.Day,
+  start=datetime.date(2022, 9, 1),
+  end=datetime.date(2022, 9, 7)
+)
+# Retrieve daily bars for Bitcoin in a DataFrame and printing it
+btc_bars = client.get_crypto_bars(request_params)
+
+# Convert to dataframe
+btc_bars.df
+print(btc_bars.df)
