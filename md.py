de72b228ed2b4dee71d431f1d7536779d40fa4a9
lamngo13 <69515114+lamngo13@users.noreply.github.com>
2026-09-13 11:33:02 -0400

    good setup for historical data

--- md.py
+++ md.py
@@ -6,54 +6,76 @@ from alpaca.data.requests import StockBarsRequest
 from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
 from alpaca.data.enums import DataFeed
 from influxdb_client_3 import InfluxDBClient3, Point
+from datetime import datetime, timedelta
+import time
 
+#creds setup
 api_key, api_secret = load_api_credentials()
-
 YOUR_INFLUX_TOKEN = load_db_secret()
 
+#client setup w creds
 client = StockHistoricalDataClient(api_key, api_secret)
 
-request = StockBarsRequest(
-    symbol_or_symbols=["VOO"],
-    timeframe=TimeFrame(1, TimeFrameUnit.Minute),
-    start="2024-01-04",
-    end="2024-01-05",
-    limit=100,
-    feed=DataFeed.IEX,
-)
-bars = client.get_stock_bars(request)
+#date setup
+first_date = datetime(2023, 1, 1)
+today = datetime.now() #this will be datetime.now() in the future
 
-print(bars.df)
+current_date = first_date
 
-df = bars.df
+#big loop to loop until today
+while current_date < today:
+    #iterate next_date
+    next_date = current_date + timedelta(days=1)
+    request = StockBarsRequest(
+        symbol_or_symbols=["SPY"],
+        timeframe=TimeFrame(1, TimeFrameUnit.Minute),
+        start=current_date.strftime("%Y-%m-%d"),
+        end=next_date.strftime("%Y-%m-%d"),
+        limit=10000, #i think its ballpark 390 anyway
+        feed=DataFeed.IEX,
+    )
+    #make request and put it in dataframe
+    bars = client.get_stock_bars(request)
+    df = bars.df
 
-influx_client = InfluxDBClient3(
-    host="http://localhost:8181",
-    database="voo",
-    token=YOUR_INFLUX_TOKEN,
-)
+    #ITERATE CURRENT DAY
+    current_date = next_date
 
-points = []
-for (symbol, timestamp), row in df.iterrows():
-    point = (
-        Point("stock_bars")
-        .tag("symbol", symbol)
-        .field("open", float(row["open"]))
-        .field("high", float(row["high"]))
-        .field("low", float(row["low"]))
-        .field("close", float(row["close"]))
-        .field("volume", float(row["volume"]))
-        .field("trade_count", float(row["trade_count"]))
-        .field("vwap", float(row["vwap"]))
-        .time(timestamp)
+    #print for fun
+    print(current_date.strftime("%Y-%m-%d"))
+    print(df)
+
+
+
+    influx_client = InfluxDBClient3(
+        host="http://localhost:8181",
+        database="voo",
+        token=YOUR_INFLUX_TOKEN,
     )
 
-    points.append(point)
+    points = []
+    for (symbol, timestamp), row in df.iterrows():
+        point = (
+            Point("stock_bars")
+            .tag("symbol", symbol)
+            .field("open", float(row["open"]))
+            .field("high", float(row["high"]))
+            .field("low", float(row["low"]))
+            .field("close", float(row["close"]))
+            .field("volume", float(row["volume"]))
+            .field("trade_count", float(row["trade_count"]))
+            .field("vwap", float(row["vwap"]))
+            .time(timestamp)
+        )
+
+        points.append(point)
 
 
-influx_client.write(points)
+    influx_client.write(points)
 
 
-influx_client.close()
+    influx_client.close()
 
-print(f"Wrote {len(df)} rows to InfluxDB")
+    print(f"Wrote {len(df)} rows to InfluxDB")
+    #SLEEP
+    time.sleep(15)
