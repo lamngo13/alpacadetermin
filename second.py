778dfc9776ee1a137fdaeed7fad7d24c524a7644
lamngo13 <69515114+lamngo13@users.noreply.github.com>
2026-09-18 14:59:27 -0400

    Update second.py

--- second.py
+++ second.py
@@ -16,13 +16,37 @@ client = InfluxDBClient3(
 # Day to query, in "YYYY-MM-DD" format
 target_date = "2024-01-03"
 
-query = f"""
+sample_query = f"""
     SELECT *
     FROM stock_bars
     WHERE symbol = 'SPY'
       AND time >= '{target_date}T00:00:00Z'
       AND time < '{target_date}T23:59:59Z'
     ORDER BY time
+"""
+
+query = f"""
+SELECT
+    time,
+    close,
+    future_max_high,
+    ((future_max_high - close) / close) * 100 AS future_gain_pct
+FROM (
+    SELECT
+        time,
+        close,
+        MAX(high) OVER (
+            ORDER BY time
+            RANGE BETWEEN CURRENT ROW AND INTERVAL '1 hour' FOLLOWING
+        ) AS future_max_high
+    FROM stock_bars
+    WHERE
+        symbol = 'SPY'
+        AND CAST(time AT TIME ZONE 'America/New_York' AS TIME) >= TIME '10:00:00'
+)
+WHERE
+    ((future_max_high - close) / close) * 100 >= 0.25
+ORDER BY time;
 """
 
 result = client.query(query)
